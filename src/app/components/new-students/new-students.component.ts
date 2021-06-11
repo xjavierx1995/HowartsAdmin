45424e7258619247b9e3264f79b1be8961ae5f8b
Javier Rodriguez <javier.rodriguez@geor.pe>
2021-06-11 01:27:43 -0400

    CRUD de nuevas solicitudes

--- src/app/components/new-students/new-students.component.ts
+++ src/app/components/new-students/new-students.component.ts
@@ -59,7 +59,18 @@ export class NewStudentsComponent implements OnInit {
   }
 
   editStudent(item){
-    console.log(item);
+    const dialogRef = this.dialog.open( ModalNewStudentComponent, {
+      width: '400px',
+      data: item
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+
+      if (result.type === 'submit') {
+        this.spinner.show()
+        this.updateStudent(result);
+      }
+    });
 
   }
   deleteStudent(item){
@@ -71,5 +82,18 @@ export class NewStudentsComponent implements OnInit {
       verticalPosition: 'top',
       duration: 5000
     })
+  }
+
+  updateStudent(item){
+    this.students[item.index] = item.params;
+    localStorage.setItem('students', JSON.stringify(this.students));
+    this.getStudents();
+    this.spinner.hide();
+    this.snack.open('Solicitud editada con exito', 'ok', {
+      horizontalPosition: 'end',
+      verticalPosition: 'top',
+      duration: 5000
+    })
+
   }
 }
--- src/app/shared/modal-new-student/modal-new-student.component.ts
+++ src/app/shared/modal-new-student/modal-new-student.component.ts
@@ -1,8 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import * as moment from 'moment';
 
+export interface ExternalData {
+  dateOfBirth: string;
+  image: string;
+  index: number;
+  name: string;
+  patronus: string;
+}
 @Component({
   selector: 'app-modal-new-student',
   templateUrl: './modal-new-student.component.html',
@@ -31,26 +38,46 @@ export class ModalNewStudentComponent implements OnInit {
 
   img: any = "https://images.squarespace-cdn.com/content/v1/54bdbaa8e4b06fad9ba682ed/1501533847974-588ZUA6ZEW2M734U2C4B/ke17ZwdGBToddI8pDm48kEO3Xd74g0bqvk55M9KUV357gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QHyNOqBUUEtDDsRWrJLTmEZprBMNaW6dUYQUrCwR2cS0Aly4N5B3QpIFZ53UruWiU8Sr9h4jG1LHLXD3lG8IY/harry+potter+avatar+for+web-01-01.jpg?format=2500w"
   fileImg: any;
+  buttonActionTitle: string = "Editar";
   constructor(
     public dialogRef: MatDialogRef<ModalNewStudentComponent>,
     private formBuilder: FormBuilder,
+    @Inject(MAT_DIALOG_DATA) public externalData: ExternalData
   ) { }
 
   ngOnInit(): void {
-    this.validations();
+
+
+    if (this.externalData == null) {
+      this.buttonActionTitle = 'Guardar';
+      this.externalData = {
+        dateOfBirth: '',
+        image: '',
+        index: null,
+        name: '',
+        patronus: ''
+      }
+
+      this.validations();
+    }else{
+      this.validations();
+      this.img = this.externalData.image
+      this.validations_form.controls['image'].clearValidators()
+      this.validations_form.controls['image'].updateValueAndValidity()
+    }
   }
 
   validations(){
     this.validations_form = this.formBuilder.group({
-      name: new FormControl('', Validators.compose([
+      name: new FormControl(this.externalData.name, Validators.compose([
         Validators.required,
         Validators.maxLength(255),
       ])),
-      patronus: new FormControl('', Validators.compose([
+      patronus: new FormControl(this.externalData.patronus, Validators.compose([
         // Validators.required,
         // Validators.maxLength(255),
       ])),
-      birthDay: new FormControl('', Validators.compose([
+      birthDay: new FormControl(this.externalData.dateOfBirth.split("-").reverse().join("-"), Validators.compose([
         Validators.required,
         Validators.maxLength(255),
       ])),
@@ -68,6 +95,10 @@ export class ModalNewStudentComponent implements OnInit {
       patronus: form.patronus,
       image: this.img,
       dateOfBirth: moment(form.birthDay).format('DD-MM-yyyy')
+    }
+
+    if (this.externalData != null) {
+      form.index = this.externalData.index
     }
     this.dialogRef.close(form);
   }
